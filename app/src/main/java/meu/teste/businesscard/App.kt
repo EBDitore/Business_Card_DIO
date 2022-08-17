6a7962d3a24e17c24fad6f27d4fe664011a04715
Elielson <ebditore@gmail.com>
2022-08-17 18:36:30 -0300

    AppDatabase e BusinessCardRepository adicionados

--- app/src/main/java/meu/teste/businesscard/App.kt
+++ app/src/main/java/meu/teste/businesscard/App.kt
@@ -1,6 +1,11 @@
 package meu.teste.businesscard
 
 import android.app.Application
+import com.google.android.datatransport.runtime.dagger.Lazy
+import meu.teste.businesscard.data.AppDatabase
+import meu.teste.businesscard.data.BusinessCardRepository
 
 class App : Application(){
+    val database by Lazy {AppDatabase.getDatabase(this) }
+    val repository by Lazy { BusinessCardRepository(database.businessDao()) }
 }
--- app/src/main/java/meu/teste/businesscard/data/BusinessCardRepository.kt
+++ app/src/main/java/meu/teste/businesscard/data/BusinessCardRepository.kt
@@ -1,4 +1,17 @@
 package meu.teste.businesscard.data
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 class BusinessCardRepository (private val dao: BusinessCardDao){ // Injeta o BusinessCardDao através da 'variável constante' dao
+
+    fun insert(businessCard: BusinessCard) = runBlocking {
+        launch(Dispatchers.IO) {
+            dao.insert(businessCard)
+        }
+    }
+
+    fun getAll() = dao.getAll()
+
 }
