596a8463d49f54e9f28814cbd26665c34696d919
Elielson <ebditore@gmail.com>
2022-08-16 22:36:57 -0300

    Binding activity_add_business_card adicionada

--- app/src/main/java/meu/teste/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/meu/teste/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,10 +2,16 @@ package meu.teste.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import meu.teste.businesscard.databinding.ActivityAddBusinessCardBinding
+import meu.teste.businesscard.databinding.ActivityMainBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddBusinessCardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_business_card)
+        binding = ActivityAddBusinessCardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
